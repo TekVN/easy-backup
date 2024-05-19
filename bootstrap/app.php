9f3ed516d04f7e0eb112e88775975b84225ca315
DNT <ducconit@gmail.com>
2024-05-19 21:05:02 +0700

    feat(api): đăng ký tài khoản

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -14,5 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->dontReportDuplicates()
+            ->shouldRenderJsonWhen(function () {
+                return true;
+            });
     })->create();
